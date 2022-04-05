27d8daf94bfaa425253295988866c4ebc05c58e5
Ishan Sharma <1905614@kiit.ac.in>
2022-04-05 16:44:50 +0530

    axios and fetch

---
+++ reactapicalls/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Axios from "axios";
+import React, { useState } from "react";
+
+function App() {
+  const [something, setSomething] = useState("");
+
+  const getSomething = () => {
+    Axios.get(`https://www.boredapi.com/api/activity`).then(
+      (res)=>{
+        setSomething(res.data.activity);
+      }
+    )
+    // fetch(`https://www.boredapi.com/api/activity`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setSomething(data.activity);
+    //   });
+  };
+  return (
+    <div>
+      <button onClick={getSomething}>I'm Bored</button>
+      {something}
+    </div>
+  );
+}
+
+export default App;
